92fe2762a4f03982c0684ac4d8f6d1e1bf77e9e1
Carlos Monteiro <unomonteiro@gmail.com>
2016-05-30 21:15:40 +0100

    First Flag

---
+++ app/src/main/java/io/monteirodev/learnflags/model/Flag.java
@@ -0,0 +1,8 @@
+package io.monteirodev.learnflags.model;
+
+public class Flag {
+    private String mNationality;
+    private String mColorOne;
+    private String mColorTwo;
+    private String mColorThree;
+}
